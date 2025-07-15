571e461da5ae6fb6d02fdf30212e2a58654369d0
Wai Kien <kienong2000@gmail.com>
2025-07-15 22:26:00 +0800

    removefromarray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter(item => !args.includes(item))
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
