d55f2807aed749ab72f7e071d47c662b24cfb5aa
Wai Kien <kienong2000@gmail.com>
2025-07-15 22:41:17 +0800

    sumall

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (
+        typeof start != 'number' || typeof end != 'number' ||
+        start < 0 || end < 0 ||
+        !Number.isInteger(start) ||
+        !Number.isInteger(end)
+    ) {
+        return 'ERROR';
+    }
+    
+    const min = Math.min(start, end)
+    const max = Math.max(start, end)
 
+    const n = (max - min) + 1
+    return n * (min + max) / 2
 };
 
 // Do not edit below this line
