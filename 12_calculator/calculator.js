db8484f974808f969e7b68f145dbb9aff975bff5
Wai Kien <kienong2000@gmail.com>
2025-08-19 22:42:42 +0800

    calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 | n === 1){
+    return 1;
+  } else if (n < 0) {
+    return "Factorial is not defined for negative numbers!";
+  } else {
+    return n * (factorial(n -1));
+  }
 };
 
 // Do not edit below this line
